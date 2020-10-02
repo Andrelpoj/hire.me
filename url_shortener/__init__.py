394e4404413c646c95cb9ca520893e48da2adf7a
André Pereira <andrelpoj@gmail.com>
2020-10-02 13:16:20 -0300

    GET and POST mocks with Flask Restful

--- url_shortener/__init__.py
+++ url_shortener/__init__.py
@@ -1,8 +1,12 @@
 from flask import Flask
+from flask_restful import Resource, Api
+from webargs import fields
+from webargs.flaskparser import use_args
 import markdown 
 import os 
 
 app = Flask(__name__)
+api = Api(app)
 
 @app.route("/")
 def index():
@@ -11,3 +15,13 @@ def index():
     with open(os.path.dirname(app.root_path) + '/README.md', 'r') as readme:
         content = readme.read()
         return markdown.markdown(content)
+
+class Shortened_URL(Resource):
+    def get(self, alias):
+        return {'message': 'success', 'alias': alias}, 200
+
+    @use_args({'alias': fields.Str(required=True)}, location='query')
+    def post(self, args):
+        return {'message': 'success', 'alias': args['alias']}, 201
+
+api.add_resource(Shortened_URL, '/u', '/u/<alias>')

899a7f82cc2fe17d204cd0bcef75f5755f72984b
André Pereira <andrelpoj@gmail.com>
2020-10-06 15:15:19 -0300

    Refactor - URLShortenerClient returns

--- url_shortener_client.py
+++ url_shortener_client.py
@@ -8,8 +8,10 @@ class URLShortenerClient():
         
         if response.status_code == 404:
             raise AliasNotFound(alias)
-            
-        return response
+        elif response.status_code == 200:
+            return response.url
+        else:
+            raise UnexpectedServerResponse(response) 
     
     def shorten_url(url, alias=None):
         req_url = URLShortenerClient.service_url + f'/addlink?url={url}'
@@ -20,12 +22,25 @@ class URLShortenerClient():
 
         if response.status_code == 400:
             raise AliasAlreadyExists(alias)
+        elif response.status_code == 201:
+            return response.json()['alias']
+        else:
+            raise UnexpectedServerResponse(response)
 
-        return response
     
     def get_top_links():        
         response = requests.get(URLShortenerClient.service_url + '/top')
-        return response
+        
+        if response.status_code == 200:            
+            top_links = response.json()
+
+            ordered_list = []
+            for alias in top_links.keys():
+                ordered_list.append( (alias, top_links[alias]) )
+            
+            return ordered_list
+        else:
+            raise UnexpectedServerResponse(response)
 
 class AliasNotFound(Exception):
     def __init__(self, alias):
@@ -34,3 +49,7 @@ class AliasNotFound(Exception):
 class AliasAlreadyExists(Exception):
     def __init__(self, alias):
         self.alias = alias 
+
+class UnexpectedServerResponse(Exception):
+    def __init__(self, response):
+        self.response = response
